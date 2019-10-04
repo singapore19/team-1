ef0ac077601439bd8e7dc810d5a2d55fc5b77e3f
amoslgl96 <baolgl96@gmail.com>
2019-10-04 12:48:04 +0800

    landing page structure ( incomplete )

--- server/client/src/router/AppRouter.js
+++ server/client/src/router/AppRouter.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import PublicRoute from './PublicRoute';
-
+import React from "react";
+import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import PublicRoute from "./PublicRoute";
+import LandingPage from "../Pages/LandingPage";
 
 import createHistory from "history/createBrowserHistory";
-import Forms from '../Pages/ReportForms/Forms';
+import Forms from "../Pages/ReportForms/Forms";
 
-export const history=createHistory();
+export const history = createHistory();
 
-//the props history will be passed down to all the components within it 
+//the props history will be passed down to all the components within it
 //The original BrowserRouter automates the passing down for initially.
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-      <PublicRoute path="/" component={Forms} exact={true} />
-        {/* <PublicRoute path="/" component={Default} exact={true} />
-        <PublicRoute path="/report" component={DefaultTwo} exact={true} /> */}
+        <PublicRoute path="/" component={Forms} exact={true} />
+        <PublicRoute path="/landing" component={LandingPage} exact={true} />
       </Switch>
     </div>
   </Router>
